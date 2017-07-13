aa35f3a61f8122aa5432012dd9a7677d36303232
haykam821 <haykam821@users.noreply.github.com>
2017-07-12 22:29:19 -0400

    Bump!

---
+++ pop.js
@@ -0,0 +1,57 @@
+var servers = JSON.parse('{"Vanilla": {"Silicon Valley": ["45.63.92.197"]},"FFA": {}}');
+
+var ge = document.getElementById("gamemodes");
+var re = document.getElementById("regions");
+var se = document.getElementById("servers");
+
+console.log(servers)
+
+for (i in Object.keys(servers)){
+	var option = document.createElement("option");
+	option.text = Object.keys(servers)[i];
+  option.value = Object.keys(servers)[i];
+
+	ge.add(option);
+}
+
+function updateRegions() {
+  clearServers();
+
+  for (i in servers[ge.value]) {
+  	var option = document.createElement("option");
+		option.text = i;
+  	option.value = i;
+
+		re.add(option);
+  }
+}
+
+function updateServers() {
+  clearServers();
+
+  for (i in servers[ge.value][re.value]) {
+  	var option = document.createElement("option");
+
+		option.text = servers[ge.value][re.value][i];
+  	option.value = servers[ge.value][re.value][i];
+
+		se.add(option);
+  }
+}
+
+function clearServers() {
+	var length = se.options.length;
+	for (i = 0; i < length; i++) {
+  	if (se.options[i].className.split(' ').indexOf('info')==-1) {
+	  	se.options[i] = null;
+    }
+	}
+}
+
+function setSelectedServer() {
+	document.getElementById("result").innerHTML = se.value
+}
+
+ge.onchange = updateRegions;
+re.onchange = updateServers;
+se.onchange = setSelectedServer;
