1ead8bad491fb66f5e61ea65f1c251386a5810c3
haykam821 <haykam821@users.noreply.github.com>
2017-07-12 22:01:20 -0400

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,2 @@
+document.getElementById('guideCard').removeChild(document.getElementsByClassName('menuHeader')[0]);
+document.getElementById('guideCard').removeChild(document.getElementById('serverSelect'));
