3774ff74bc386d942926641310b349929d444270
haykam821 <haykam821@users.noreply.github.com>
2017-07-19 11:23:34 -0400

    Dynamic disable without permissions for all tabs

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
     var url = info.url || tab.url;
-    if (url && url.indexOf('moomoo.io') == -1) {
+    /*if (url && url.indexOf('moomoo.io') == -1) {
+        chrome.browserAction.disable(tabId);
+    } else {
+        chrome.browserAction.enable(tabId);
+    }*/
+    if (url == null) {
         chrome.browserAction.disable(tabId);
     } else {
         chrome.browserAction.enable(tabId);
