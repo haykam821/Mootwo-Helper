bc99b2a84e945b0c7eea75967c3aa2df2364f1e5
haykam821 <haykam821@users.noreply.github.com>
2017-07-21 11:18:17 -0400

    Move all Chrome extension files to 'chrome' folder

---
+++ chrome/inject.js
@@ -0,0 +1,4 @@
+document.getElementById('guideCard').removeChild(document.getElementsByClassName('menuHeader')[0]);
+document.getElementById('guideCard').removeChild(document.getElementById('serverSelect'));
+
+document.getElementsByClassName('menuHeader')[0].style.marginTop = "0px";
---
+++ chrome/pop.js
@@ -0,0 +1,77 @@
+var servers = {};
+
+var ge = document.getElementById("gamemodes");
+var re = document.getElementById("regions");
+var se = document.getElementById("servers");
+
+$.getJSON('https://cdn.rawgit.com/haykam821/Mootwo-Helper/ac701af3/servers.json', function(data) {
+	servers = data;
+
+	for (i in Object.keys(servers)){
+		var option = document.createElement("option");
+		option.text = Object.keys(servers)[i];
+	  option.value = Object.keys(servers)[i];
+
+		ge.add(option);
+	}
+});
+
+function updateRegions() {
+  clearServers();
+
+  for (i in servers[ge.value]) {
+  	var option = document.createElement("option");
+		option.text = i;
+  	option.value = i;
+
+		re.add(option);
+  }
+
+	re.value = 'info';
+	se.value = 'info';
+}
+
+function updateServers() {
+  clearServers();
+
+  for (i in servers[ge.value][re.value]) {
+  	var option = document.createElement("option");
+
+		option.text = servers[ge.value][re.value][i];
+  	option.value = servers[ge.value][re.value][i];
+
+		se.add(option);
+  }
+
+	se.value = 'info';
+}
+
+function clearRegions() {
+	var length = re.options.length;
+	for (i = 0; i < length; i++) {
+  	if (re.options[i].className.split(' ').indexOf('info')==-1) {
+	  	re.options[i] = null;
+    }
+	}
+}
+
+function clearServers() {
+	var length = se.options.length;
+	for (i = 0; i < length; i++) {
+  	if (se.options[i].className.split(' ').indexOf('info')==-1) {
+	  	se.options[i] = null;
+    }
+	}
+}
+
+function setSelectedServer() {
+	document.getElementById("result").innerHTML = se.value;
+
+	chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+      chrome.tabs.update(tab.id, {url: `https://moomoo.io/?party=${se.value}`});
+	});
+}
+
+ge.onchange = updateRegions;
+re.onchange = updateServers;
+se.onchange = setSelectedServer;
