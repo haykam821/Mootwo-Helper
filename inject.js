a431bbc81c3141f9d2cdf79d635d061fcb652860
haykam821 <haykam821@users.noreply.github.com>
2017-07-21 11:16:32 -0400

    Delete inject.js

--- inject.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('guideCard').removeChild(document.getElementsByClassName('menuHeader')[0]);
-document.getElementById('guideCard').removeChild(document.getElementById('serverSelect'));
-
-document.getElementsByClassName('menuHeader')[0].style.marginTop = "0px";
